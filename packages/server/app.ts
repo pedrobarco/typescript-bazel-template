d221ae05a0189f8ab0d6c202e9a9a4bead738302
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2022-04-26 23:19:20 +0100

    feat(server): add app class

---
+++ packages/server/app.ts
@@ -0,0 +1,47 @@
+import { Operation } from "@usrbinboat/core";
+import type { Application, Request, Response } from "express";
+import express from "express";
+
+export interface AppProps {
+    operation: Operation;
+    port: number;
+}
+
+type AppOptions = Partial<AppProps> & {
+    operation: Operation;
+};
+
+export class App {
+    private app: Application;
+    private port: number;
+
+    private constructor(props: AppProps) {
+        this.app = express();
+        this.port = props.port;
+        this.routes(props.operation);
+    }
+
+    private routes(operation: Operation) {
+        this.app.get("/", (_: Request, res: Response) => {
+            const num1 = Math.floor(Math.random() * 10);
+            const num2 = Math.floor(Math.random() * 10);
+            const str = operation.stringify(num1, num2);
+            res.send(str);
+        });
+    }
+
+    public start() {
+        this.app.listen(this.port, () => {
+            console.log(`server started at http://localhost:${this.port}`);
+        });
+    }
+
+    static create(options: AppOptions): App {
+        const props: AppProps = {
+            ...options,
+            port: options.port || 8080,
+        };
+
+        return new App(props);
+    }
+}
---
+++ packages/server/index.ts
@@ -0,0 +1,9 @@
+import { Add } from "@usrbinboat/add";
+import { App } from "./app";
+
+const app = App.create({
+    operation: new Add(),
+    port: 8080,
+});
+
+app.start();
